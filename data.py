6ed31f57fb56929f39d7c39764226437b336dbad
pirmil <151455672+pirmil@users.noreply.github.com>
2023-11-25 16:03:48 +0100

    Update data.py

--- data.py
+++ data.py
@@ -1,32 +1,21 @@
 import torchvision.transforms as transforms
 
 # once the images are loaded, how do we pre-process them before being passed into the network
-# by default, we resize the images to 64 x 64 in size
+# by default, we resize the images to 224 x 224 in size because this is the expected image size
+# for the fine-tuned models AlexNet, ResNet and VGG
 # and normalize them to mean = 0 and standard-deviation = 1 based on statistics collected from ImageNet
-data_transforms = transforms.Compose([
-    transforms.Resize((64, 64)),
-    transforms.ToTensor(),
-    transforms.Normalize(
-        mean=[0.485, 0.456, 0.406],
-        std=[0.229, 0.224, 0.225]
-    )
-])
 
-data_transforms_VGG = transforms.Compose([
-    transforms.Resize((128, 128)),
+# Validation and test data
+data_transforms_valid = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.CenterCrop((224, 224)),
     transforms.ToTensor(),
-    transforms.Normalize(
-        mean=[0.485, 0.456, 0.406],
-        std=[0.229, 0.224, 0.225]
-    )
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ])
-
-data_transforms_VGG_train_flip = transforms.Compose([
-    transforms.RandomHorizontalFlip(p=1), # flip the image with proba = 1
-    transforms.Resize((128, 128)),
+# Training data: data augmentation with RandomResizedCrop and RandomHorizontalFlip
+data_transforms_train = transforms.Compose([
+    transforms.RandomResizedCrop((224, 224)),
+    transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
-    transforms.Normalize(
-        mean=[0.485, 0.456, 0.406],
-        std=[0.229, 0.224, 0.225]
-    )
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ])
